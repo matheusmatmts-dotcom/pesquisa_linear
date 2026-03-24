2241793ff64fd6d0df580175532e68136464ac34
Matheus Mattos <matheusmat.mts@gmail.com>
2026-03-24 20:23:25 -0300

    Update Pesqiusa_linear.java

--- src/main/java/Pesqiusa_linear.java
+++ src/main/java/Pesqiusa_linear.java
@@ -16,6 +16,7 @@ public class Pesqiusa_linear {
             if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
                 JOptionPane.showMessageDialog(null, "o valor procurado foi encontrado na posição " + i);
                 flag = 1;
+                i=numElementos
             }
         }
         if (flag == 0) {
